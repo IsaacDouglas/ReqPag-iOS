de33e83757ee6f10ae82f5f00a28bff07a62307a
Isaac Douglas <isaacdouglas2015@gmail.com>
2019-02-09 17:06:31 -0300

    first commit

---
+++ General/View/Splash/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SplashViewController.swift
+//  recPag
+//
+//  Created by Isaac Douglas on 09/02/19.
+//  Copyright © 2019 Isaac Douglas. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# ReqPag-iOS
